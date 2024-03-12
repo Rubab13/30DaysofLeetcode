687f50529adea258b2249a2e575a05c4507e27e9
Rubab13 <137704457+Rubab13@users.noreply.github.com>
2024-03-12 12:29:01 +0500

    Time: 2 ms (74.11%) | Memory: 54.5 MB (30.47%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,34 +1,26 @@
 class Solution {
-  public static int[] productExceptSelf(int[] array) {
-    int[] answer = new int[array.length];
-    int[] leftProduct = new int[array.length];
-    int[] rightProduct = new int[array.length];
-
-    // Calculate left product
-    leftProduct[0] = 1;
-    for (int i = 1; i < array.length; i++) {
-      leftProduct[i] = leftProduct[i - 1] * array[i - 1];
+    public static int[] productExceptSelf(int[] array) {
+        int[] answer = new int[array.length];
+        int[] left = new int[array.length];
+        int[] right = new int[array.length];
+        left[0] = 1;
+        for (int i = 1; i < array.length; i++) {
+            left[i] = left[i - 1] * array[i - 1];
+        }
+        right[array.length - 1] = 1;
+        for (int i = array.length - 2; i >= 0; i--) {
+            right[i] = right[i + 1] * array[i + 1];
+        }
+        for (int i = 0; i < array.length; i++) {
+            answer[i] = left[i] * right[i];
+        }
+        return answer;
     }
-
-    // Calculate right product
-    rightProduct[array.length - 1] = 1;
-    for (int i = array.length - 2; i >= 0; i--) {
-      rightProduct[i] = rightProduct[i + 1] * array[i + 1];
-    }
-
-    // Calculate final product
-    for (int i = 0; i < array.length; i++) {
-      answer[i] = leftProduct[i] * rightProduct[i];
-    }
-
-    return answer;
-  }
-
-  public static void main(String[] args) {
-    int[] array = {-1, 1, 0, -3, 3};
-    int[] result = productExceptSelf(array);
-    for (int i = 0; i < result.length; i++) {
-      System.out.print(result[i] + " ");
+    public static void main(String[] args) {
+        int[] array = {-1, 1, 0, -3, 3};
+        int[] result = productExceptSelf(array);
+        for (int i = 0; i < result.length; i++) {
+        System.out.print(result[i] + " ");
     }
   }
 }
