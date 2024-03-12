6703d78326846b9cfcf078ab985a8602911a8e06
Rubab13 <137704457+Rubab13@users.noreply.github.com>
2024-03-12 12:34:05 +0500

    Time: 2 ms (74.11%) | Memory: 54.9 MB (19.27%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -4,16 +4,20 @@ class Solution {
         int[] left = new int[array.length];
         int[] right = new int[array.length];
         left[0] = 1;
+
         for (int i = 1; i < array.length; i++) {
             left[i] = left[i - 1] * array[i - 1];
         }
+
         right[array.length - 1] = 1;
         for (int i = array.length - 2; i >= 0; i--) {
             right[i] = right[i + 1] * array[i + 1];
         }
+
         for (int i = 0; i < array.length; i++) {
             answer[i] = left[i] * right[i];
         }
+        
         return answer;
     }
     public static void main(String[] args) {
