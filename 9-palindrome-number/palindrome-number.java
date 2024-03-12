50e94d6386092b4a7702b8facf2210e0adeff77b
Rubab13 <137704457+Rubab13@users.noreply.github.com>
2024-03-12 19:13:22 +0500

    Time: 23 ms (5.87%) | Memory: 44.9 MB (5.53%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public static void main(String[] args) {
+        if (isPalindrome(121)) {
+            System.out.println("The given string is a palindrome.");
+        }
+        else{
+            System.out.println("The given string is not a palindrome.");
+        }
+    }
+    
+    public static boolean isPalindrome(int x) {
+        char ch;
+        String result = "";
+        String string = Integer.toString(x);
+        for (int i = 0; i < string.length(); i++) {
+            ch = string.charAt(i);
+            result = ch + result;
+        }
+        return (string.equals(result)) ? true : false;
+    }
+}
